4e3940b98ad9f72437562d3f7f35ebec3c688351
nico3003m <mjhon0181@gmail.com>
2024-05-07 19:58:06 -0500

    feat(nosotros): ajustes

--- graficasBacata/src/app/components/footer/footer.component.ts
+++ graficasBacata/src/app/components/footer/footer.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  router: any;
 
-  constructor() { }
+  constructor(router:Router) {
+    this.router = router;
 
-  ngOnInit(): void {
+   }
+
+  ngOnInit(): void {3
+  const correo = 'servicliente@graficasbacata.com.co'
   }
 
+  navigate(path:string){
+    this.router.navigate([path])
+  }
 }
