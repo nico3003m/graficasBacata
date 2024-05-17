e1ceb0699ebc5f0c9447274faf51d58f8fff52e8
Andres Huerfano <andresfelipehuerfano.99@gmail.com>
2024-05-16 21:30:05 -0500

    0.1.5

--- graficasBacata/src/app/proyecto/pages/servicios/servicios.component.ts
+++ graficasBacata/src/app/proyecto/pages/servicios/servicios.component.ts
@@ -12,4 +12,53 @@ export class ServiciosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  services = [
+    {
+      title: 'Tarjetas',
+      image: './../../../../assets/img/Tarjetas.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      title: 'CORTE LASER',
+      image: './../../../../assets/img/Volantes.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      expanded: false
+    },
+    {
+      title: 'CORTE LASER',
+      image: './../../../../assets/img/Sellos.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      expanded: false
+    },
+    {
+      title: 'Posillos',
+      image: './../../../../assets/img/Posillos.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      expanded: false
+    },
+    {
+      title: 'Cuadernos',
+      image: './../../../../assets/img/Cuadernos.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      expanded: false
+    },
+    {
+      title: 'Brochures',
+      image: './../../../../assets/img/Brochures.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      expanded: false
+    },
+  ];
+
+  toggleReadMore(service: any) {
+    service.expanded = !service.expanded;
+  }
+
+  shouldAddDots(text: string, limit: number, isExpanded: boolean): string {
+    if (!isExpanded && text.length > limit) {
+      return text.slice(0, limit) + '...';
+    }
+    return text;
+  }
+  
 }
